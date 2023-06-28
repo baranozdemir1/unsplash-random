b5da223edc93f7756bcc629030e8d49d772ba256
Baran Özdemir <info@baranozdemir.com>
2023-06-28 17:52:22 +0300

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const axios = require("axios").default;
+
+const app = express();
+
+const unsplashAccessKey = "Uffd5I4F_ngX6I9BBtbtB_tEeZtfi-uHAUV00M5o1-4";
+const unsplashSecretKey = "mjbc5NN-idGRdioO6PmWUJftc3igRS2DXTzQUcHiIM8";
+
+app.get("/", async (req, res) => {
+  let fullUrl = req.originalUrl;
+  let queryString = fullUrl.split("?")[1];
+
+  const photo = await axios.get(
+    `https://api.unsplash.com/photos/random/?client_id=${unsplashAccessKey}`,
+    {
+      params: {
+        query: queryString,
+        count: 1,
+      },
+    }
+  );
+
+  res.redirect(photo.data[0].urls.raw + "&w=800&h=552");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
